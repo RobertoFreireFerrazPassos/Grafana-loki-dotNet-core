f78036e71e9fc5151a8a217617916d3d65e541e1
Roberto Freire <robertofreireferrazpassos@gmail.com>
2022-06-25 09:30:29 -0300

    Added promtail but not working yet

--- Log/Logger.cs
+++ Log/Logger.cs
@@ -7,25 +7,25 @@ namespace LogLibrary
     {
         public static void Information(object data)
         {
-            Log.ForContext("Extra", JsonSerializer.Serialize(data))
+            Log.ForContext("Extra", data)
                .Information(JsonSerializer.Serialize(data));
         }
 
         public static void Information(object data, string message)
         {
-            Log.ForContext("Extra", JsonSerializer.Serialize(data))
+            Log.ForContext("Extra", data)
                .Information(message);
         }
 
         public static void Error(Exception ex, object data, string message)
         {
-            Log.ForContext("Error", JsonSerializer.Serialize(data))
+            Log.ForContext("Error",data)
                   .Error(ex, message);
         }
 
         public static void Error(object data, string message)
         {
-            Log.ForContext("Error", JsonSerializer.Serialize(data))
+            Log.ForContext("Error", data)
                   .Information(message);
         }
     }
